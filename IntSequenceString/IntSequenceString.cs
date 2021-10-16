d014e27c50b43ffcc7f6cb5f091f08dc8110f025
Mustafa <sheikh.muhammad.mustafa@gmail.com>
2021-10-17 02:58:01 +1100

    First implementation of LongestIncreasingSubsequence

--- IntSequenceString/IntSequenceString.cs
+++ IntSequenceString/IntSequenceString.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IntSequenceString
 {
@@ -6,7 +7,34 @@ namespace IntSequenceString
     {
         public string LongestIncreasingSubsequence(string intSequenceString)
         {
-            throw new NotImplementedException("Not implemented. ");
+            int lisStartIndex = 0, currentLisStartIndex = 0;
+            int lisFinshIndex = 0, currentLisFinishIndex = 0;
+
+            int[] intSequenceArr = intSequenceString.Split(null).Select(x => int.Parse(x)).ToArray();
+
+            for (int i = 0; i < intSequenceArr.Length - 1; ++i){
+                if (intSequenceArr[i] < intSequenceArr[i + 1] ) {
+                    currentLisFinishIndex = i + 1;
+                    continue;
+                }
+                else {
+                    currentLisFinishIndex = i;
+                }
+
+                if ((lisFinshIndex - lisStartIndex) < (currentLisFinishIndex - currentLisStartIndex))
+                {
+                    lisFinshIndex = currentLisFinishIndex;
+                    lisStartIndex = currentLisStartIndex;
+                }
+
+                currentLisStartIndex = currentLisFinishIndex = i + 1;
+            }
+
+            ArraySegment<int> segment = new ArraySegment<int>(intSequenceArr, lisStartIndex, (lisFinshIndex - lisStartIndex + 1));
+
+            string result = String.Join(" ", segment.Select(p=>p.ToString()).ToArray());
+
+            return result;
         }
     }
 }
